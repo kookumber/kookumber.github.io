e9ad7760c72972ac94139c9e0fc2c40995f5bcd0
Quang Tran <quang.tran.769869@gmail.com>
2022-08-30 11:26:10 -0700

    updated og.png files

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+  `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
